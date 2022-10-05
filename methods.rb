89c9f4b6d2821e9281841d3ddbd7cce238e51ea3
kilonzif <faithmueni6@gmail.com>
2022-10-05 10:13:36 +0300

    ruby methods done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+
+def greetProgrammer() 
+  puts "Hello, programmer!";
+end
+
+
+def greet(name)
+  puts "Hello, #{name}!";
+end
+
+def greetWithDefault(name = "programmer")
+  puts "Hello, #{name}!";
+end
+
+
+def add(num1, num2) 
+  return num1 + num2;
+end
+
+
+def halve(number) 
+  if typeof number != "number"
+     return null;
+  end 
+  return number / 2;
+end
+
+greet "Fk Kilo"
